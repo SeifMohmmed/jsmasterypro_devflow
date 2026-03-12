85bd4026a7a895fff6039f309c06f6805922b22a
Seif Mohamed <seif.eissa7797@gmail.com>
2026-03-12 19:17:35 +0200

    Add Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "Devflow",
-  description: "A better version of Stack Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: "/public/images/site-logo.svg",
 };
 
 export default function RootLayout({
